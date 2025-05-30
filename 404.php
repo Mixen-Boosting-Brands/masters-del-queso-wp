724ff8fb2d77494e7692b4f0419384a7b7362028
Luis Carlos Pando <hey@luiscarlospando.com>
2025-05-30 15:01:19 -0600

    Ajustes generales

--- 404.php
+++ 404.php
@@ -3,7 +3,7 @@
     <section class="jumbotron-interna py-60">
         <div class="container">
             <div class="row">
-                <div class="col-12">
+                <div class="col-12 text-center">
                     <h1 data-aos="fade-up"
                         data-aos-duration="1000"
                         data-aos-delay="200">
