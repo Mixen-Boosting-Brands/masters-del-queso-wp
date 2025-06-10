63b4439d38985ea544f9012fd68fccf7089f7fd1
Luis Carlos Pando <hey@luiscarlospando.com>
2025-06-10 17:00:00 -0600

    Agregar modal del formulario de contacto

--- footer.php
+++ footer.php
@@ -122,7 +122,9 @@
             <div id="form-messages"></div>
 
             <form
-                action="./mailer.php"
+                action="<?php echo esc_url(
+                    get_template_directory_uri()
+                ); ?>/mailer.php"
                 method="POST"
                 class="row g-3 needs-validation contact-form mb-4 mb-lg-0"
                 id="ajax-contact"
