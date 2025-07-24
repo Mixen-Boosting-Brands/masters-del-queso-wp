9eaae89e04a10e3ecb9bbc12a9e6f178b05437f2
Luis Carlos Pando <hey@luiscarlospando.com>
2025-07-24 10:32:26 -0600

    Actualizar tamaño de thumbs del swiper

--- functions.php
+++ functions.php
@@ -28,7 +28,7 @@ if (function_exists("add_theme_support")) {
     add_image_size("medium", 250, "", true); // Medium Thumbnail.
     add_image_size("small", 120, "", true); // Small Thumbnail.
     add_image_size("thumb-producto-receta", 400, "", true);
-    add_image_size("thumb-producto-swiper", 400, 400, true);
+    add_image_size("thumb-producto-swiper", 400, 9999); //
 
     // Add Support for Custom Backgrounds - Uncomment below if you're going to use.
     /*add_theme_support('custom-background', array(
